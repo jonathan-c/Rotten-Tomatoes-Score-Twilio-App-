48de319f19704cfb465f99ac334eb4c043adfbe5
Jonathan Castillo <jonathan@ygm.com>
2012-06-14 03:04:25 -0400

    First commit

---
+++ app/helpers/send_text_helper.rb
@@ -0,0 +1,15 @@
+module SendTextHelper
+  
+  def find_movie_score(movie)
+   bf = BadFruit.new("c337mtn76ujsn6m6krkyrdp2") 
+ 	 movies = bf.movies.search_by_name(movie) 
+ 	 cast = movies[0].full_cast 
+ 	 reviews = movies[0].reviews
+ 	 scores = movies[0].scores 
+
+ 	 scores.critics_score
+ 	 #movies 
+   #cast.methods.sort
+  end
+   
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieApp::Application.config.secret_token = 'a9f8d9c59fcecbb9ddbbea6371a6d351e54d3fb7852c63d8e193c4b943baf9a6d13262b6b8af45ab8373516d0c081738c4968b5ccae0afe76cf75d637cc864ea'
