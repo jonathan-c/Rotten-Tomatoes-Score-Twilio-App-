70b4f63f0f586a103ce6318d1b657b5e2ef14818
Jonathan Castillo <jonathan@ygm.com>
2012-06-18 21:58:06 -0400

    One las try

--- app/helpers/receive_text_helper.rb
+++ app/helpers/receive_text_helper.rb
@@ -10,11 +10,11 @@ module ReceiveTextHelper
    # Uncomment when testing on the local database
    # @example = Post.create(body: "american pie", from: "+15166582879")
    
-   @request = Post.last.body.titleize
+   @request = Post.last.body
    @send_message_to = Post.last.from
    @account = @client.account
    movie_score = find_movie_score(@request)
-   if movie_score == nil
+   if return_movie_name(@request) == nil
      @message = @account.sms.messages.create({:from => '+15163368089', :to => @send_message_to, :body => "Did you type in the name of the movie correctly? Double check. It's also possible that RT does not have this movie in its records." })
       puts @message
    elsif movie_score == 1%
@@ -38,6 +38,13 @@ module ReceiveTextHelper
   	 #movies 
     #cast.methods.sort
    end
+   
+   def return_movie_name(movie)
+     bf = BadFruit.new("c337mtn76ujsn6m6krkyrdp2") 
+   	 movies = bf.movies.search_by_name(movie) 
+   	 @returned_movie_name = movies[0].name
+   	 @returned_movie_name
+    end
    
    def return_movie_thumbnail(movie)
      bf = BadFruit.new("c337mtn76ujsn6m6krkyrdp2") 
