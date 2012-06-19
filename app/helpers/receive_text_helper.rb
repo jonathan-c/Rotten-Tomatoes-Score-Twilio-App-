48811b979a2fa36c6970b490c79a2405d0cdd30a
Jonathan Castillo <jonathan@ygm.com>
2012-06-18 22:02:48 -0400

    Trying to fix n/a score response

--- app/helpers/receive_text_helper.rb
+++ app/helpers/receive_text_helper.rb
@@ -3,6 +3,7 @@ module ReceiveTextHelper
   def send_text_message
    @account_sid = 'AC8d291a7930f04a79ab486f95c8058a18'
    @auth_token = '9be6999dcd821ee41a5c1eff4a8296f6'
+   @from_number = '+15163368089'
 
    # set up a client to talk to the Twilio REST API
    @client = Twilio::REST::Client.new(@account_sid, @auth_token)
@@ -14,14 +15,11 @@ module ReceiveTextHelper
    @send_message_to = Post.last.from
    @account = @client.account
    movie_score = find_movie_score(@request)
-   if return_movie_name(@request) == nil
-     @message = @account.sms.messages.create({:from => '+15163368089', :to => @send_message_to, :body => "Did you type in the name of the movie correctly? Double check. It's also possible that RT does not have this movie in its records." })
-      puts @message
-   elsif movie_score == 1%
-     @message = @account.sms.messages.create({:from => '+15163368089', :to => @send_message_to, :body => "There is no Rotten Tomatoes critic score for this movie yet."  })
+   if movie_score == -1
+     @message = @account.sms.messages.create({:from => @from_number, :to => @send_message_to, :body => "There is no Rotten Tomatoes critic score for this movie yet."  })
       puts @message
    else
-     @message = @account.sms.messages.create({:from => '+15163368089', :to => @send_message_to, :body => "The Rotten Tomatoes score for "             + '"' + @returned_movie + '"' + " is " + movie_score + "%" })
+     @message = @account.sms.messages.create({:from => @from_number, :to => @send_message_to, :body => "The Rotten Tomatoes score for "             + '"' + @returned_movie + '"' + " is " + movie_score + "%" })
      puts @message
    end
    end
@@ -38,13 +36,6 @@ module ReceiveTextHelper
   	 #movies 
     #cast.methods.sort
    end
-   
-   def return_movie_name(movie)
-     bf = BadFruit.new("c337mtn76ujsn6m6krkyrdp2") 
-   	 movies = bf.movies.search_by_name(movie) 
-   	 @returned_movie_name = movies[0].name
-   	 @returned_movie_name
-    end
    
    def return_movie_thumbnail(movie)
      bf = BadFruit.new("c337mtn76ujsn6m6krkyrdp2") 
