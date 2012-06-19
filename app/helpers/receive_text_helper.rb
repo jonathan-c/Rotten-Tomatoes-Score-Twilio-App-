0ac702541cb7569820060e5ea607ccd4ff437688
Jonathan Castillo <jonathan@ygm.com>
2012-06-18 22:04:17 -0400

    Forgot to set it to sandbox

--- app/helpers/receive_text_helper.rb
+++ app/helpers/receive_text_helper.rb
@@ -3,6 +3,7 @@ module ReceiveTextHelper
   def send_text_message
    @account_sid = 'AC8d291a7930f04a79ab486f95c8058a18'
    @auth_token = '9be6999dcd821ee41a5c1eff4a8296f6'
+   @sandbox_number = '+14155992671'
    @from_number = '+15163368089'
 
    # set up a client to talk to the Twilio REST API
@@ -16,10 +17,10 @@ module ReceiveTextHelper
    @account = @client.account
    movie_score = find_movie_score(@request)
    if movie_score == -1
-     @message = @account.sms.messages.create({:from => @from_number, :to => @send_message_to, :body => "There is no Rotten Tomatoes critic score for this movie yet."  })
+     @message = @account.sms.messages.create({:from => @sandbox_number, :to => @send_message_to, :body => "There is no Rotten Tomatoes critic score for this movie yet."  })
       puts @message
    else
-     @message = @account.sms.messages.create({:from => @from_number, :to => @send_message_to, :body => "The Rotten Tomatoes score for "             + '"' + @returned_movie + '"' + " is " + movie_score + "%" })
+     @message = @account.sms.messages.create({:from => @sandbox_number, :to => @send_message_to, :body => "The Rotten Tomatoes score for "             + '"' + @returned_movie + '"' + " is " + movie_score + "%" })
      puts @message
    end
    end
